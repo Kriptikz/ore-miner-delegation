499d7619131be7cea7b9205eba4e7be65332fba7
Kriptikz <Kriptikz@users.noreply.github.com>
2024-06-01 15:47:23 -0500

    comments cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,16 @@ use solana_program::{
 };
 
 
+// TODO: Update id with generated key
 declare_id!("SWK6MtQGZ4NJaijbHw2UPgtuSAo3NgZoM1dGgQw2x7n");
+
 #[cfg(not(feature = "no-entrypoint"))]
 solana_program::entrypoint!(process_instruction);
 
 pub fn process_instruction(
     program_id: &Pubkey,
     _accounts: &[AccountInfo],
-    instruction_data: &[u8],
+    _instruction_data: &[u8],
 ) -> ProgramResult {
     if program_id.ne(&crate::id()) {
         return Err(ProgramError::IncorrectProgramId);
@@ -31,18 +33,6 @@ pub fn process_instruction(
     //     Instructions::InitWorld => {
     //         msg!("Instruction Init World");
     //         process_init_world(accounts, data)?;
-    //     }
-    //     Instructions::CreateLand => {
-    //         msg!("Instruction Create Land");
-    //         process_create_land(accounts, data)?;
-    //     }
-    //     Instructions::InitIsland => {
-    //         msg!("Instruction Init Island");
-    //         process_init_island(accounts, data)?;
-    //     }
-    //     Instructions::AddCampsite => {
-    //         msg!("Instruction Add Campsite");
-    //         process_add_campsite(accounts, data)?;
     //     }
     // }
 
