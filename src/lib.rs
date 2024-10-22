21428fa3b52020813b3b5d01ca8e62b5c63bb3b9
Kriptikz <Kriptikz@users.noreply.github.com>
2024-10-22 16:58:08 -0500

    move instruction processors to processor module

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,18 @@
-use instruction::{init_delegate_boost, undelegate_boost, Instructions};
+use instruction::Instructions;
 use solana_program::{
     account_info::AccountInfo, declare_id, entrypoint::ProgramResult, program_error::ProgramError,
     pubkey::Pubkey,
 };
 
+mod processor;
+
 pub mod consts;
-pub mod delegate_stake;
 pub mod error;
-pub mod init_delegate_stake;
 pub mod instruction;
 pub mod loaders;
-pub mod mine;
-pub mod open_managed_proof;
 pub mod pda;
 pub mod state;
-pub mod undelegate_stake;
 pub mod utils;
-pub mod open_managed_proof_boost;
-pub mod delegate_boost;
-pub mod undelegate_boost;
-pub mod init_delegate_boost;
-pub mod init_delegate_boost_v2;
-pub mod delegate_boost_v2;
-pub mod undelegate_boost_v2;
-pub mod migrate_delegate_boost_to_v2;
 
 declare_id!("J6XAzG8S5KmoBM8GcCFfF8NmtzD7U3QPnbhNiYwsu9we");
 
@@ -48,43 +37,43 @@ pub fn process_instruction(
 
     match instruction {
         Instructions::OpenManagedProof => {
-            open_managed_proof::process_open_managed_proof(accounts, data)?;
+            processor::open_managed_proof::process_open_managed_proof(accounts, data)?;
         }
         Instructions::Mine => {
-            mine::process_mine(accounts, data)?;
+            processor::mine::process_mine(accounts, data)?;
         }
         Instructions::InitDelegateStake => {
-            init_delegate_stake::process_init_delegate_stake(accounts, data)?;
+            processor::init_delegate_stake::process_init_delegate_stake(accounts, data)?;
         }
         Instructions::DelegateStake => {
-            delegate_stake::process_delegate_stake(accounts, data)?;
+            processor::delegate_stake::process_delegate_stake(accounts, data)?;
         }
         Instructions::UndelegateStake => {
-            undelegate_stake::process_undelegate_stake(accounts, data)?;
+            processor::undelegate_stake::process_undelegate_stake(accounts, data)?;
         }
         Instructions::OpenManagedProofBoost => {
-            open_managed_proof_boost::process_open_managed_proof_boost(accounts, data)?;
+            processor::open_managed_proof_boost::process_open_managed_proof_boost(accounts, data)?;
         }
         Instructions::InitDelegateBoost => {
-            init_delegate_boost::process_init_delegate_boost(accounts, data)?;
+            processor::init_delegate_boost::process_init_delegate_boost(accounts, data)?;
         }
         Instructions::DelegateBoost => {
-            delegate_boost::process_delegate_boost(accounts, data)?;
+            processor::delegate_boost::process_delegate_boost(accounts, data)?;
         }
         Instructions::UndelegateBoost => {
-            undelegate_boost::process_undelegate_boost(accounts, data)?;
+            processor::undelegate_boost::process_undelegate_boost(accounts, data)?;
         }
         Instructions::InitDelegateBoostV2 => {
-            init_delegate_boost_v2::process_init_delegate_boost_v2(accounts, data)?;
+            processor::init_delegate_boost_v2::process_init_delegate_boost_v2(accounts, data)?;
         }
         Instructions::DelegateBoostV2 => {
-            delegate_boost_v2::process_delegate_boost_v2(accounts, data)?;
+            processor::delegate_boost_v2::process_delegate_boost_v2(accounts, data)?;
         }
         Instructions::UndelegateBoostV2 => {
-            undelegate_boost_v2::process_undelegate_boost_v2(accounts, data)?;
+            processor::undelegate_boost_v2::process_undelegate_boost_v2(accounts, data)?;
         }
         Instructions::MigrateDelegateBoostToV2 => {
-            migrate_delegate_boost_to_v2::process_migrate_delegate_boost_v2(accounts, data)?;
+            processor::migrate_delegate_boost_to_v2::process_migrate_delegate_boost_v2(accounts, data)?;
         }
     }
 
---
+++ src/processor/mod.rs
@@ -0,0 +1,13 @@
+pub mod delegate_boost;
+pub mod delegate_boost_v2;
+pub mod delegate_stake;
+pub mod init_delegate_boost;
+pub mod init_delegate_boost_v2;
+pub mod init_delegate_stake;
+pub mod migrate_delegate_boost_to_v2;
+pub mod mine;
+pub mod open_managed_proof;
+pub mod open_managed_proof_boost;
+pub mod undelegate_boost;
+pub mod undelegate_boost_v2;
+pub mod undelegate_stake;
